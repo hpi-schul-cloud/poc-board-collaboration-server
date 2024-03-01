f7bbde3ef2e90958d016904159be4d8b38a053cf
hoeppner-dataport <marcus.hoeppner@dataport.de>
2024-03-01 11:54:46 +0100

    switched to request-success-messages

--- src/socket/socket/socket.gateway.ts
+++ src/socket/socket/socket.gateway.ts
@@ -32,24 +32,27 @@ export class SocketGateway
     this.logger.log(`Cliend id:${client.id} disconnected`);
   }
 
-  @SubscribeMessage('update-card')
+  @SubscribeMessage('update-card-request')
   handleUpdateCard(client: any, data: any) {
     this.logger.log(`Message received from client id: ${client.id}`);
     this.logger.debug(`Payload: ${data}`);
-    client.broadcast.emit('update-card', data);
+    client.broadcast.emit('update-card-success', data);
+    client.emit('update-card-success', data);
   }
 
-  @SubscribeMessage('lock-card')
+  @SubscribeMessage('lock-card-request')
   handleLockCard(client: any, data: any) {
     this.logger.log(`Message received from client id: ${client.id}`);
     this.logger.debug(`Payload: ${data}`);
-    client.broadcast.emit('lock-card', data);
+    client.broadcast.emit('lock-card-success', data);
+    client.emit('lock-card-success', data);
   }
 
-  @SubscribeMessage('unlock-card')
+  @SubscribeMessage('unlock-card-request')
   handleUnlockCard(client: any, data: any) {
     this.logger.log(`Message received from client id: ${client.id}`);
     this.logger.debug(`Payload: ${data}`);
-    client.broadcast.emit('unlock-card', data);
+    client.broadcast.emit('unlock-card-success', data);
+    client.emit('unlock-card-success', data);
   }
 }
