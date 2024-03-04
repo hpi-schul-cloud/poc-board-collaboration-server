1133ad09252f015232ac3f5f64872f1877255405
hoeppner-dataport <marcus.hoeppner@dataport.de>
2024-03-04 11:50:00 +0100

    set global prefix (aka base path)

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
     cors: true,
   });
+  app.setGlobalPrefix('bc-6683-poc-board-collaboration');
   await app.listen(3000); // TODO: use env variable
 }
 bootstrap();
