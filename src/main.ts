60103fa55b9e8bb844990d6cb5631eb2ae0249b4
Thomas Feldtkeller <thomas.feldtkeller@dataport.de>
2024-03-27 14:50:54 +0100

    add redis adapter

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { RedisIoAdapter } from './socket/socket/redis-adapter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
     cors: true,
   });
   app.setGlobalPrefix('bc-6683-poc-board-collaboration');
+
+  const redisIoAdapter = new RedisIoAdapter(app);
+  await redisIoAdapter.connectToRedis();
+
+  app.useWebSocketAdapter(redisIoAdapter);
   await app.listen(3000); // TODO: use env variable
 }
 bootstrap();
---
+++ src/socket/socket/redis-adapter.ts
@@ -0,0 +1,28 @@
+import { IoAdapter } from '@nestjs/platform-socket.io';
+import { ServerOptions } from 'socket.io';
+import { createAdapter } from '@socket.io/redis-adapter';
+import { createClient } from 'redis';
+
+export class RedisIoAdapter extends IoAdapter {
+  private adapterConstructor: ReturnType<typeof createAdapter>;
+
+  async connectToRedis(): Promise<void> {
+	if (process.env.REDIS_URI) {
+		console.log('found a secret redis uri')
+	} else {
+		console.log('did not find a secret redis uri')
+	}
+    const pubClient = createClient({ url: process.env.REDIS_URI || `redis://localhost:6379` });
+    const subClient = pubClient.duplicate();
+
+    await Promise.all([pubClient.connect(), subClient.connect()]);
+
+    this.adapterConstructor = createAdapter(pubClient, subClient);
+  }
+
+  createIOServer(port: number, options?: ServerOptions): any {
+    const server = super.createIOServer(port, options);
+    server.adapter(this.adapterConstructor);
+    return server;
+  }
+}
